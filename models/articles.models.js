52567b581a442a43c749881ee72eb2a5a06424d0
Nacershucks <zack.sawyerr@gmail.com>
2024-02-21 20:10:41 +0000

    removed consolelogs

--- models/articles.models.js
+++ models/articles.models.js
@@ -33,22 +33,20 @@ exports.selectArticles = () => {
 }
 
 exports.updateArticle = (articleId, updateBody) => {
-    console.log(articleId);
-    console.log(updateBody);
     if(typeof updateBody.inc_votes !== 'number' || !Object.keys(updateBody).includes('inc_votes')){
         return Promise.reject({status: 400, msg: 'Bad Request'})
     }
+
     const queryString = format(`UPDATE articles SET votes = votes + %L WHERE article_id = %L RETURNING *`,
                                 updateBody.inc_votes,
                                 articleId.article_id
                                 )
-    console.log(queryString);
+                                
     return db.query(queryString)
     .then((result) => {
         if (result.rows.length === 0){
             return Promise.reject({status: 404, msg: "Not Found"})
         }
-        console.log('in query result');
         return result.rows[0]
     })
     .catch((err) => {
