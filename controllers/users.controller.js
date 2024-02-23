44efaadf7892bea2423c20dc6aa22f5f5311f08d
Nacershucks <zack.sawyerr@gmail.com>
2024-02-23 15:54:59 +0000

    implements get user by username

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,4 @@
-const { selectUsers } = require("../models/users.models")
+const { selectUsers, selectUser } = require("../models/users.models")
 
 
 
@@ -6,5 +6,15 @@ exports.getUsers = (req, res, next) => {
     selectUsers()
     .then((users) => {
         res.status(200).send({users})
+    })
+}
+
+exports.getUserByUsername = (req, res, next) => {
+    selectUser(req.params)
+    .then((user) => {
+        res.status(200).send(user)
+    })
+    .catch((err) => {
+        next(err)
     })
 }
--- models/users.models.js
+++ models/users.models.js
@@ -11,3 +11,20 @@ exports.selectUsers = () => {
         return rows
     })
 }
+
+exports.selectUser = ({username}) => {
+    const queryString = format(`SELECT username,
+                        name,
+                        avatar_url
+                        FROM users
+                        WHERE username = %L`,
+                        username 
+                        )
+    return db.query(queryString)
+    .then(({rows}) => {
+        if(rows[0] === undefined){
+            return Promise.reject({status: 404, msg : 'Not Found'})
+        } 
+        return rows[0]
+    })
+}
