a32b0e528317e4099a43381634f71e6017fcc42f
Nacershucks <zack.sawyerr@gmail.com>
2024-03-11 11:00:52 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const { handleInvalidEndpoint, handleInternalServerError, handlePSQLErrors, handleCustomErrors } = require('./controllers/errors.controller')
 
 const apiRouter = require('./routers/api.router')
+
+app.use(cors())
 
 app.use(express.json())
 
